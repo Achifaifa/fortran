b6a54e0a14a8f575701fed342562172a8139f48f
Yuri Numerov <achi@hush.ai>
2014-12-27 00:25:23 +0100

    added adding add.f95

---
+++ add.f95
@@ -0,0 +1,9 @@
+program add
+integer::x,y
+print *,'enter two numbers'
+print *,'first'
+read *,x
+print *,'second'
+read *,y
+print *,'Total: ',x+y
+end program add
---
+++ readme.md
@@ -0,0 +1,1 @@
+Fortran practice and fiddling programs
